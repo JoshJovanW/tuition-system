00fbcbf0169887e8b8d6112ba9d5cce23133cd58
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-11-05 02:06:06 -0400

    Update program.py

--- program.py
+++ program.py
@@ -10,15 +10,13 @@ class Program:
         self.logged_in_as_student = ''
         self.logged_in_as_tutor = ''
 
-    def staff_login(self, id, password):
+    def staff_login(self, username, password):
         staff = Staff()
-        if id == staff.username and password == staff.password:
+        if username == staff.username and password == staff.password:
             self.logged_in_as = 'staff'
             return "Successfully logged in as staff"
-    
-    def staff_menu(self):
-        print("\nWelcome to Staff Menu\n" + "-" * 25)
-        print("Select a Staff Menu: \n0. Log Out\n1. Register a tutor \n2. Register a student\n3. Update a student subject\n4. Search Payment made by Student\n")
+        
+        
 
     def register_tutor(self, name, age, subject, phoneNumber):
         tutor = Tutor(name, age, subject, phoneNumber)
@@ -39,6 +37,42 @@ class Program:
         if updated == False:
             return "Student name not found. Please enter a valid student"
 
+    def staff_menu(self, option):
+        print("\nWelcome to Staff Menu\n" + "-" * 25)
+        option = input("Select a Staff Menu: \n0. Log Out\n1. Register a tutor \n2. Register a student\n3. Update a student subject\n4. Search Payment made by Student\n")
+     
+        if option == 1:
+            name = input("Tutor Name    : ")
+            age = int(input("Tutor Age     : "))
+            subject = input("Subject       : ")
+            phoneNumber = input("Phone Number  : ")
+
+            print(Program.register_tutor(name, age, subject, phoneNumber))
+            return "Successfully registered Tutor"
+
+        elif option == 2:
+            name = input("Student Name                    : ")
+            ic = input("IC/Passport                     : ")
+            email = input("Student Email                   : ")
+            phoneNumber = input("Contact Number                  : ")
+            address = input("Student Address                 : ")
+            level = input("Level                           : ")
+            subject = input("Subjects(max 3 format: math,science,economics)                 : ")
+
+            subjects = subject.split(",")
+            print(Program.register_student(name,ic,email,phoneNumber,address,level,subjects))
+            return "Succesfully registered Student"
+        
+        elif option == 3: 
+            name = input("Student Name                    : ")
+            subject = input("Subjects(max 3 format: math,science,economics)                 : ")
+
+            subjects = subject.split(",")
+            print(Program.update_student_subject(name, subjects))
+
+        elif option == 4:
+            pass
+
     def student_login(self, name):
         if self.students != {}:
             for students in self.students:
@@ -50,10 +84,6 @@ class Program:
         if self.logged_in_as != 'student':
             return "Error, failed to login. Please try again."
 
-    def student_menu(self):
-        print("\nWelcome to Student Menu\n" + "-" * 25)
-        print("Select a Student Menu: \n0. Log Out\n1. Pay tuition fees\n2. Update their information\n3. Check schedule of classes\n")
-    
     def pay_tuition(self, amount):
         if self.logged_in_as == 'student':
             self.students[self.logged_in_as_student].pay_tuition_fees(amount)
@@ -62,6 +92,24 @@ class Program:
     def update_information(self, element, information):
         if self.logged_in_as == 'student':
             self.students[self.logged_in_as_student].update_information(element, information)
+
+    def student_menu(self):
+        print("\nWelcome to Student Menu\n" + "-" * 25)
+        option = input("Select a Student Menu: \n0. Log Out\n1. Pay tuition fees\n2. Update their information\n3. Check schedule of classes\n")
+        
+        if option == 1:
+            amount = int(input("How much do you want to pay?(RM)    "))
+            print(Program.pay_tuition(amount))
+
+        elif option == 2:
+            element = input("What element do you want to update? (email, phoneNumber, address) \n")
+            information = input("What do you want to change it to? \n")
+            print(Program.update_information(element, information))
+            
+        elif option == 3:
+            pass
+
+        
     
     def tutor_login(self, name):
         if self.tutors != {}:
@@ -74,10 +122,6 @@ class Program:
         if self.logged_in_as != 'tutor':
             return "Error, failed to login. Please try again."
 
-    def tutor_menu(self):
-        print("\nWelcome to Tutor Menu\n" + "-" * 25)
-        print("Select a Student Menu: \n0. Log Out\n1. Add class information\n2. Update class information\n3. View Profiles of students\n")
-   
     def add_class_information(self, subject, price, schedule):
         if self.logged_in_as == 'tutor':
             self.tutors[self.logged_in_as_tutor].add_class_information(subject, price, schedule)
@@ -89,3 +133,17 @@ class Program:
     def view_student_profile(self):
         if self.logged_in_as == 'tutor':
             self.tutors[self.logged_in_as_tutor].view_profile()
+
+    def tutor_menu(self):
+        print("\nWelcome to Tutor Menu\n" + "-" * 25)
+        option = input("Select a Student Menu: \n0. Log Out\n1. Add class information\n2. Update class information\n3. View Profiles of students\n")
+
+        if option == 1:
+            pass
+        
+        elif option == 2:
+            pass
+
+        elif option == 3:
+            pass
+        
