414d4ae62ad22d1148ca183a5e4be4b45f6d990f
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-11-06 03:45:47 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, name, ic, email, phoneNumber, address, grade, subjects):
+    def __init__(self, name, ic, email, phoneNumber, address, grade, subjects, ):
         self.name = name
         self.ic = ic
         self.email = email
@@ -7,27 +7,33 @@ class Student:
         self.address = address
         self.grade = grade
         self.subjects = subjects
-        self.payment = 0
+        self.payment = []
+        self.classes = []
+        
         
 
     def update_information(self, element, input):
-        if element == "email":
-            self.email = input
-            return "Your email has been updated"
+        try:
+            if element == "email":
+                self.email = input
+                return "Your email has been updated"
 
-        elif element == "phoneNumber":
-            self.phoneNumber = input
-            return "Your phoneNumber has been updated"
-        
-        elif element == "address":
-            self.address = input
-            return "your Address had been updated"
+            elif element == "phoneNumber":
+                self.phoneNumber = input
+                return "Your phoneNumber has been updated"
+            
+            elif element == "address":
+                self.address = input
+                return "your Address had been updated"
 
-        else:
+        except:
             return "Error, the element you wanted to update does not exist or is restricted. Please try again"
 
     def pay_tuition_fees(self, amount):
-        self.payment += amount
+        self.payment.append(amount) 
+
+
+    
     
     
 
