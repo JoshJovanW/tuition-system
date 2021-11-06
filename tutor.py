98ce18ea9c22bec4db6ac4b82327b26d0a1ecd7e
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-11-06 03:45:28 -0400

    Update tutor.py

--- tutor.py
+++ tutor.py
@@ -8,21 +8,26 @@ class Tutor:
         self.classroom = ''
 
     def add_class_information(self, subject, charges, schedule):
-        classroom = Classroom(subject, charges, schedule)
-        self.classroom = classroom
-        return "You have succesfully added the class information"
+        try:
+            classroom = Classroom(subject, charges, schedule)
+            self.classroom = classroom
+            return "You have succesfully added the class information"
+        
+        except:
+            return "An Error has occured. Please Try again."
 
     def update_class_information(self, element, information):
-        if self.classroom != '':
-            self.classroom.update_information(element, information)
+        try:
+            if self.classroom != '':
+                self.classroom.update_information(element, information)
+            return f"The element {element} has been updated."
 
-        else:
+        except:
             return "Error, Class information has not been added. Please add the class information first."
-        
 
     def view_profile(self):
-        if self.classroom != '':
-            self.classroom.see_students()
-
-        else:
-            return "Error, Class information has not been added. Please add the class information first."
+        try:
+            if self.classroom != '':
+                self.classroom.see_students()
+        except:
+            return "Error, There are no students enrolled in this class."
